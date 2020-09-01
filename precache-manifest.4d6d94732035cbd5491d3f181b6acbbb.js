448593ea4ab09a47a2ae6467052c521664d7ed9e
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-09-01 02:54:18 +0200

    Updates

---
+++ precache-manifest.4d6d94732035cbd5491d3f181b6acbbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86075892c656b9221065269063f5e75",
+    "url": "/project1end/index.html"
+  },
+  {
+    "revision": "41e7b44c7aa19e8ac5ac",
+    "url": "/project1end/static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "0d7b94437083d2284a57",
+    "url": "/project1end/static/css/main.ab21f45f.chunk.css"
+  },
+  {
+    "revision": "41e7b44c7aa19e8ac5ac",
+    "url": "/project1end/static/js/2.0ba50841.chunk.js"
+  },
+  {
+    "revision": "59ed23295286e2e103014ddcf563fb08",
+    "url": "/project1end/static/js/2.0ba50841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7b94437083d2284a57",
+    "url": "/project1end/static/js/main.305aedc0.chunk.js"
+  },
+  {
+    "revision": "472038f041bd7b1f5277",
+    "url": "/project1end/static/js/runtime-main.01bb17bd.js"
+  }
+]);
